ca9a121a6883c7ccb9fb37b838ae2d4aa6bd3969
Nick Hackman <snickhackman@gmail.com>
2019-05-29 13:22:31 -0400

    Normalizing paths is now crossplatform
    * Stopped using std::fs::canonicalize in favor of a custom
    implementation, because std::fs::canonicalize requires paths to exist
    and therefore all globs fail

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,6 @@ pub mod sort;
 pub mod ui;
 
 use shellexpand::full;
-use std::fs::canonicalize;
 use std::path::PathBuf;
 
 /// Converts the provided path by user to a path that can be glob'd, note this function takes the
@@ -33,9 +32,11 @@ use std::path::PathBuf;
 /// Environment variables, like ~ and $HOME, are expanded
 /// On Unix escaped spaces are removed for example: folder\ path -> folder path
 /// Directories are changed from /home/etc to /home/etc/*
+/// Symlinks are followed
 pub fn path_to_glob(current_dir: &PathBuf, path: &str) -> Result<PathBuf, String> {
     const ESCAPED_SPACE: &str = r"\ ";
     const SPACE: &str = " ";
+    const GLOB: &str = "*";
 
     let mut expanded_path = match full(path) {
         Ok(path) => {
@@ -53,47 +54,42 @@ pub fn path_to_glob(current_dir: &PathBuf, path: &str) -> Result<PathBuf, String
         expanded_path = current_dir.join(expanded_path);
     }
     // normalize path
-    let mut expanded_path = match normalize_path(expanded_path) {
-        Ok(path) => path,
-        Err(e) => return Err(e.to_string()),
-    };
+    let mut expanded_path = normalize_path(expanded_path);
     // If path is a dir, add /* to glob
     if expanded_path.is_dir() {
-        expanded_path.push("*");
+        expanded_path.push(GLOB);
     }
     Ok(expanded_path)
 }
 
 /// Normalizes paths removing "." and ".."
+/// This follows symlinks like std::fs::canonicalize
 /// This is a helper function to path_to_glob
-fn normalize_path(path: PathBuf) -> Result<PathBuf, String> {
-    if let Ok(path) = canonicalize(&path) {
-        Ok(path)
-    } else {
-        // if canonicalize failed it's most likely because the path contains '*'s
-        // remove those and see if it is successful, and add them back on if it is
-        use std::ffi::OsStr;
+///
+///
+/// This implementation is preferred to using std::fs::canonicalize due to canonicalize requiring
+/// the path to exist, causing all globbing to fail and having to remove the glob, normalize then add it back on
+/// which is quadratic time complexity versus linear in the size of the path
+fn normalize_path(path: PathBuf) -> PathBuf {
+    use std::fs::read_link;
+    use std::path::Component;
 
-        let mut stack: Vec<&OsStr> = Vec::new();
-        let mut invalid_expanded_path = path.clone();
-        for parent in path.ancestors() {
-            if let Some(child) = parent.file_name() {
-                if invalid_expanded_path.exists() {
-                    break;
-                }
-                stack.push(child);
-            } else {
-                // parent is '..' remove it
-                invalid_expanded_path.pop();
+    let mut normalized = PathBuf::new();
+    for component in path.components() {
+        match component {
+            Component::ParentDir => {
+                normalized.pop();
             }
-            invalid_expanded_path.pop();
+            Component::CurDir => continue,
+            _ => {
+                normalized.push(component);
+                if let Ok(actual_path) = read_link(&normalized) {
+                    normalized.set_file_name(actual_path);
+                }
+            }
         }
-        let mut new_path = canonicalize(&invalid_expanded_path).map_err(|e| e.to_string())?;
-        while let Some(sub_path) = stack.pop() {
-            new_path.push(sub_path);
-        }
-        Ok(new_path)
     }
+    normalized
 }
 
 /// Takes in the output of path_to_glob and finds the closest parent in that path
